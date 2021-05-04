83f80d0603f111e50ac04a3ce706d488f7e9a06e
Ross Osborne <r.osborne@live.co.uk>
2021-05-05 00:14:19 +0100

    Initial project commit - area selection in progress (currently works left to right but not in reverse)

---
+++ Blazor.ImageClient/wwwroot/scripts/getOffsets2.js
@@ -0,0 +1,8 @@
+﻿function getOffsets2 (id) {
+    var element = document.getElementById(id);
+    var boundingBox = element.getBoundingClientRect();
+    return {
+        x: boundingBox.left,
+        y: boundingBox.top
+    };
+}
